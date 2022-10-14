9281c62e067e67fda74ff95f93bb51f116b44602
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-14 23:50:48 +0300

    add commits and finish task 1

--- hw2/task2_1.py
+++ hw2/task2_1.py
@@ -4,3 +4,23 @@
 
 # - 6782 -> 23
 # - 0,56 -> 11
+
+num = list(input('Input number: '))
+num.sort()
+x = num.count(',')
+y = num.count('.')
+num.reverse()
+if x > 0 or y > 0:
+    num.pop()
+
+sum=0
+h=0
+i=0
+
+while i < len(num):
+    h=int(num[i])
+
+    sum+=h
+    i=i+1
+
+print('Sum of digits= ', sum)
