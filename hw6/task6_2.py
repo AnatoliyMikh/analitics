0febe8780b966718a10e0a3a93643117d0a1b404
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-10-29 16:43:23 +0300

    task 2 complete

---
+++ hw6/task6_2.py
@@ -0,0 +1,36 @@
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+# Пример:
+
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+
+def write_file(mn):
+    f = open('task2.txt','w')
+    f.write(mn)
+    f.close()
+
+k = int(input('input k:'))
+sp = list()
+i=0
+while i < 20:
+    t = int(random.uniform(0,100)) 
+    sp.append(t)
+    i += 1
+print(str(sp))                          # просмотр полученного списка случайных чисел
+
+s = str(f'{random.choice(sp)}*x^{k}+')
+
+p = k
+j = 0
+while j < p-1:
+    s += str(f'{random.choice(sp)}x^{k-1}+')
+    j += 1
+    k -= 1
+    
+s += str(f'{random.choice(sp)}=0')
+
+print(s)                                # проверка полученного многочлена
+write_file(s)
+
